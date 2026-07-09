8eb4f325969e22cdefbf9d5f217a51af8f7cb7f9
Your Name <you@example.com>
2026-07-09 19:42:05 +0100

    fix: convert embedded submodule to a standard directory

---
+++ addons/equipment_gest/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Enterprise Equipment Management',
+    'version': '1.0',
+    'category': 'Enterprise Equipment Management',
+    'sequence': -100,
+    'description': 'Enterprise Equipment Management',
+    'author': 'Your Company',
+    'website': 'https://www.yourcompany.com',
+    'depends': ['base', 'hr', 'mail'],
+    'data': [
+        'security/equipment_security.xml',
+        'security/ir.model.access.csv',
+        'views/assignment_views.xml',
+        'views/equipment_views.xml',
+        'views/menu.xml',
+    ],
+}
